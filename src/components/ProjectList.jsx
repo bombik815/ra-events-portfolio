def684bf8aeb2be59d1ed721707eddf0514486f8
bombik815 <58844959+bombik815@users.noreply.github.com>
2022-08-31 11:00:16 +0300

    Delete ProjectList.jsx

--- src/components/ProjectList.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function ProjectList(props) {
-  const { projects } = props;
-  return (
-    <div className='list'>{projects.map((el, i) => <img className='images' src={el} key={i} alt={el}/>)}</div>
-  )
-}
